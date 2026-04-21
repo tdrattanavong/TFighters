f084b3cbe9204f782312f5784846cbed2dad378a
Taihvonne Rattanavong <tdrattanavong@gmail.com>
2026-04-21 00:40:43 -0700

    Created first Fighting class Nak Muay

--- src/main/java/com/pluralsight/FightingClass.java
+++ src/main/java/com/pluralsight/FightingClass.java
@@ -2,5 +2,30 @@ package com.pluralsight;
 
 public class FightingClass
 
+
 {
+//String
+
+// Declaring varibles
+    String fightingStyles;
+    String fightingAttribute1;
+    String fightingAttribute2;
+    String fightingAttribute3;
+
+
+
+
+public FightingClass(String fightingStyle, String fightingAttribute1, String fightingAttribute2, String fightingAttribute3){
+    This.fightingStyles = fightingStyle;
+    This.fightingAttribute1 = fightingAttribute1;
+    This.fightingAttribute2 = fightingAttribute2;
+    This.fightingAttribute3 = fightingAttribute3;
+
+
+
+
+}
+
+
+
 }
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,22 +1,27 @@
 package com.pluralsight;
+
+import java.lang.reflect.Array;
+
 public class Main {
     static void main() {
 
 // User picks a fighting class
 
+        int[]columns = {6};
 
-        String sku = columns[0];
-        String name = columns[1];
-        double price = columns[2];
-        String department = columns[3];
-
-        pv7849|iphone18|1199.99
+        String sku;
+        String name ;
+        double price ;
+        String department;
 
 
-       MuayThai|HardHitting|Slow|Tanky
-       Karate|Technical|Fast|fragile
-       Taekwondo|Precision|Fast|Sustainable
 
+
+//      MuayThai|HardHitting|Slow|Tank
+//       Karate|Technical|Fast|fragile
+//       Taekwondo|Precision|Fast|Sustainable
+
+    FightingClass nak_muay = new FightingClass("Muay Thai",  "Hard-Hitting","Slow", "Tanky");
 
 
     }
