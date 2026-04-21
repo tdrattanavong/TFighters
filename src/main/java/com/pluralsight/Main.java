78d1785d4b6d679b27ed0972e4ca64b24ae77b29
Taihvonne Rattanavong <tdrattanavong@gmail.com>
2026-04-20 23:40:43 -0700

    first commit

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+public class Main {
+    static void main() {
+
+// User picks a fighting class
+
+
+        String sku = columns[0];
+        String name = columns[1];
+        double price = columns[2];
+        String department = columns[3];
+
+        pv7849|iphone18|1199.99
+
+
+       MuayThai|HardHitting|Slow|Tanky
+       Karate|Technical|Fast|fragile
+       Taekwondo|Precision|Fast|Sustainable
+
+
+
+    }
+}
